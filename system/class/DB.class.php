f49678306542cf4a5b22a44fe5508fd0909bb2bf
Matt Pelmear <mjpelmear@gmail.com>
2011-03-11 00:27:24 -0500

    Some work on DB.class

--- system/class/DB.class.php
+++ system/class/DB.class.php
@@ -1,5 +1,12 @@
 <?php
 
+	/**
+	 * Ember DB base class
+	 * @version 1.0
+	 * @package Ember
+	 * @subpackage DB
+	 * @author Matt Pelmear <mjpelmear@gmail.com>
+	 */
 	class DB
 	{
 		const MODE_AUTO			= 'AUTO';
@@ -8,54 +15,88 @@
 
 		const ERR_NAME_EMPTY_STRING		= 'DB Connector Name cannot be an empty string';
 		const ERR_INVALID_DBCONN_MODE	= 'Invalid DB connection mode: "%mode%"';
+		const ERR_NO_SUCH_CONNECTION	= 'No such DB connection: "%name%" (%mode%)';
 
 		static private $connections = array();
 		static private $default_conn = NULL;
 		static private $tz = NULL;
 
-		static public function getCol( $sql, $col=0, $db=NULL )
+		static public function getCol( $sql, $db=NULL )
 		{
+			return DB::doDBcall( 'getCol', $sql, $db );
+		}
 		
+		static public function getColN( $sql, $col, $db=NULL )
+		{
+//TODO: getColN()
 		}
 
 		static public function getAll( $sql, $db=NULL )
 		{
-		
+			return DB::doDBcall( 'getAll', $sql, $db );
 		}
 
 		static public function getRow( $sql, $db=NULL )
 		{
-		
+			return DB::doDBcall( 'getRow', $sql, $db );
 		}
 
 		static public function getAssoc( $sql, $db=NULL )
 		{
-		
+			return DB::doDBcall( 'getAssoc', $sql, $db );
 		}
 
 		static public function getOne( $sql, $db=NULL )
 		{
-		
+			return DB::doDBcall( 'getOne', $sql, $db );
 		}
 
 		static public function query( $sql, $db=NULL )
 		{
-		
+			return DB::doDBcall( 'query', $sql, $db );
 		}
 
 		static public function lowPriorityQuery( $sql, $db=NULL )
 		{
-		
+//TODO: DB::lowPriorityQuery()
 		}
 
-		static public function getLastInsertID( $db=NULL )
+		/**
+		 * Returns the insert ID from the last query that was run.
+		 * @params String $db
+		 * @return Int
+		 */
+		static public function getLastInsertID( $db='default' )
 		{
-		
+			if( !isset(DB::$connections[$db][DB::MODE_READWRITE]) )
+				throw new Exception( str_replace( '%name%', $db,
+						str_replace( '%mode%', $mode,
+						DB::ERR_NO_SUCH_CONNECTION )));
+
+			return DB::$connections[$db][DB::MODE_READWRITE]->getLastInsertID();
 		}
 
+		static public function doDBcall( $method, $sql, $db )
+		{
+//TODO: DB::doDBcall()
+		}
+
+		/**
+		 * @param String $db (Optional; Defaults to NULL, meaning the default connector)
+		 * @param String $mode (See DB::MODE_READWRITE, DB::MODE_READONLY)
+		 * @return Bool
+		 */
 		static public function isConnected( $db=NULL, $mode=DB::MODE_READWRITE )
 		{
-		
+			if( $db === NULL )
+				$db = 'default';
+			
+			if( !isset(DB::$connections[$name][$mode]) )
+				throw new Exception( str_replace( '%name%', $db,
+						str_replace( '%mode%', $mode,
+						DB::ERR_NO_SUCH_CONNECTION )));
+
+			return DB::$connections[$name][$mode]->isConnected();
 		}
 
 		/**
@@ -108,14 +149,69 @@
 				DB::setDefault( $name );
 		}
 
+		/**
+		 * Removes a connector from the pool of available connectors.
+		 * If you remove the READONLY connection, this will remove all READONLY connectors.
+		 * @param String $name
+		 * @param String $mode (See DB::MODE_READWRITE, DB::MODE_READONLY)
+		 */
 		static public function removeConnection( $name, $mode )
 		{
-		
+//TODO: What to do when we remove the default connector?
+			$name = (String) $name;
+			switch( $mode )
+			{
+				case DB::MODE_READWRITE:
+					if( isset(DB::$connections[$name][DB::MODE_READWRITE]) )
+						DB::$connections[$name][DB::MODE_READWRITE]->disconnect();
+					else
+						throw new Exception( str_replace( '%name%', $name,
+								str_replace( '%mode%', DB::MODE_READWRITE,
+								DB::ERR_NO_SUCH_CONNECTION )));
+					break;
+
+				case DB::MODE_READONLY:
+					if( isset(DB::$connections[$name][DB::MODE_READONLY]) )
+					{
+						foreach( DB::$connections[$name][DB::MODE_READONLY]['con'] as $db )
+							$db->disconnect();
+					}
+					else
+						throw new Exception( str_replace( '%name%', $name,
+								str_replace( '%mode%', DB::MODE_READONLY,
+								DB::ERR_NO_SUCH_CONNECTION )));
+					break;
+
+				default:
+					throw new Exception( str_replace( '%mode%', $mode, DB::ERR_INVALID_DBCONN_MODE ) );
+			}
 		}
 
+		/**
+		 * Disconnects all connectors
+		 */
+		static public function disconnectAll()
+		{
+			foreach( DB::$connections as $conn )
+			{
+				if( isset($conn[DB::MODE_READWRITE]) )
+					$conn[DB::MODE_READWRITE]->disconnect();
+				if( isset($conn[DB::MODE_READONLY]) )
+				{
+					foreach( $conn[DB::MODE_READONLY]['con'] as $ro )
+						$ro->disconnect();
+				}
+			}
+		}
+
+		/**
+		 * Sets connector with name $name as the default to be used.
+		 * @param String $name
+		 */
 		static public function setDefault( $name )
 		{
-
+			DB::$default_conn = $name;
+			DB::$connections['default'] =& DB::$connections[$name];
 		}
 	}
 
